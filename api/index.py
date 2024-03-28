0dc0f4ee1d33d74330bc7b007abe8612c49f2d75
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-27 20:50:16 -0400

    templates

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
+@app.route("/")
+def home():
+    return render_template("homePage.html")
+
+
+@app.route("/about")
 def about():
-    return 'About'
+    return "About"
