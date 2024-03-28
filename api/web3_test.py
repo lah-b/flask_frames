0ed335dcae0024ad9644a05eff0771a1fd6c0361
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-28 18:52:53 -0400

    render yessir

--- api/web3_test.py
+++ api/web3_test.py
@@ -1,9 +1,9 @@
 from web3 import web3
 import os
 
-alchemy_base_rpc = os.environ.get("ALCHEMY_BASE_RPC")
-w3 = web3.Web3(web3.HTTPProvider(alchemy_base_rpc))
+# alchemy_base_rpc = os.environ.get("ALCHEMY_BASE_RPC")
+# w3 = web3.Web3(web3.HTTPProvider(alchemy_base_rpc))
 
 
 def is_w3_connected():
-    return w3.is_connected()
+    return "Yessir"
