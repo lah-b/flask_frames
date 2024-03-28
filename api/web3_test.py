bf017688582065f14f1406748e64a725f9a0c7fe
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-28 18:59:22 -0400

    add w3

--- api/web3_test.py
+++ api/web3_test.py
@@ -1,9 +1,13 @@
-from web3 import web3
+from web3 import Web3
 import os
 
-# alchemy_base_rpc = os.environ.get("ALCHEMY_BASE_RPC")
-# w3 = web3.Web3(web3.HTTPProvider(alchemy_base_rpc))
+alchemy_base_rpc = os.environ.get("ALCHEMY_BASE_RPC")
+w3 = Web3(Web3.HTTPProvider(alchemy_base_rpc))
 
 
 def is_w3_connected():
-    return "Yessir"
+    return w3.is_connected()
+
+
+if __name__ == "__main__":
+    print(is_w3_connected())
