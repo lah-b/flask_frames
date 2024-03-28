105a09858459f736a0b82b65a51e27b5f1e7e8c2
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-28 18:41:07 -0400

    comment out w3

--- api/index.py
+++ api/index.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-from web3_test import is_w3_connected
+
+# from web3_test import is_w3_connected
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    print(f"Is web3 connected {is_w3_connected()}")
+    # print(f"Is web3 connected {is_w3_connected()}")
     return render_template("homePage.html")
 
 
