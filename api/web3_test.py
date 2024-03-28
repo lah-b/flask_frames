11cf553f8e7651062e39b5691ddfb070c5127eb7
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-28 19:01:08 -0400

    no w3

--- api/web3_test.py
+++ api/web3_test.py
@@ -1,13 +1,13 @@
-from web3 import Web3
-import os
+# from web3 import Web3
+# import os
 
-alchemy_base_rpc = os.environ.get("ALCHEMY_BASE_RPC")
-w3 = Web3(Web3.HTTPProvider(alchemy_base_rpc))
+# alchemy_base_rpc = os.environ.get("ALCHEMY_BASE_RPC")
+# w3 = Web3(Web3.HTTPProvider(alchemy_base_rpc))
 
 
 def is_w3_connected():
-    return w3.is_connected()
+    return "testing"
 
 
-if __name__ == "__main__":
-    print(is_w3_connected())
+# if __name__ == "__main__":
+#     print(is_w3_connected())
