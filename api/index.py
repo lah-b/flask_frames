3ec20704b0a174ff53c037a090445fb5d517b79f
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-28 18:36:28 -0400

    use vercel secrets

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from web3_test import is_w3_connected
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+    print(f"Is web3 connected {is_w3_connected()}")
     return render_template("homePage.html")
 
 
