86777e4828490e4cffbea1ba5122ffb4d0011c18
lahiffbill <lahiffbill@gmail.com>
2024-03-29 12:56:32 +0000

    remore is_w3_connected

--- api/index.py
+++ api/index.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-from web3_test import is_w3_connected
+# from web3_test import is_w3_connected
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    w3_cnnected = is_w3_connected()
-    return render_template("homePage.html", name="Dawg", w3_connect=w3_cnnected)
+    # w3_cnnected = is_w3_connected()
+    return render_template("homePage.html", name="Dawg", w3_connect="yessir")
 
 
 @app.route("/about")
