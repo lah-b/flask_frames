60ad44ba7913def523d4edf7972987f700729638
lahiffbill <lahiffbill@gmail.com>
2024-03-29 16:34:30 +0000

    make repo

--- api/web3_test.py
+++ api/web3_test.py
@@ -1,6 +1,6 @@
 import os
 # from web3 import Web3
-# w3 = Web3(Web3.HTTPProvider("https://base-sepolia.g.alchemy.com/v2/pvnBbBibAjgLf2FQRSRa8-jnXO65Jtyf"))
+
 my_var = os.environ.get("MY_VAR")
 def is_w3_connected():
     return my_var
