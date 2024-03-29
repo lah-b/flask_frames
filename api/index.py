fa92648f9f02021d2222c1a33c2e1e7e2bcd86b0
lahiffbill <lahiffbill@gmail.com>
2024-03-29 19:20:02 +0000

    edit post route

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ from .web3_test import is_w3_connected
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     w3_response = is_w3_connected()
@@ -14,9 +15,19 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/mint-post', methods=['POST'])
+
+@app.route("/mint-post", methods=["POST"])
 def parse_request():
     post_data = request.json  # Assuming JSON data is posted
     # Process the post_data
-    render_string = post_data['name']
-    return f"your name is {render_string}"
+    render_string = post_data["name"]
+    data = {
+        chainId: "eip155:10",
+        method: "eth_sendTransaction",
+        params: {
+            to: "0x00000000fcCe7f938e7aE6D3c335bD6a1a7c593D",
+            data: render_string,
+            value: "984316556204476",
+        },
+    }
+    return jsonify(data), 200
