384dcacd02b24ccb2418c1eb6b1b57799c407d40
lahiffbill <lahiffbill@gmail.com>
2024-03-29 16:55:06 +0000

    return block number

--- api/web3_test.py
+++ api/web3_test.py
@@ -4,4 +4,6 @@ from web3 import Web3
 alchemy_rpc = os.environ.get("ALCHEMY_BASE_RPC")
 def is_w3_connected():
     w3 = Web3(Web3.HTTPProvider(alchemy_rpc))
-    return w3.is_connected()
+    data = w3.eth.get_block('latest')
+
+    return data['number']
