f2dfb7a59ef976e3bd5fd623a5f423c60762274a
lahiffbill <lahiffbill@gmail.com>
2024-03-29 13:23:55 +0000

    use monorepo

--- api/index.py
+++ api/index.py
@@ -4,10 +4,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def is_w3_connected():
+    return "yes"
+
 @app.route("/")
 def home():
-    # w3_response = is_w3_connected()
-    return render_template("homePage.html", name="Dawg", w3_connect="yes maam")
+    w3_response = is_w3_connected()
+    return render_template("homePage.html", name="Dawg", w3_connect=w3_response)
 
 
 @app.route("/about")
