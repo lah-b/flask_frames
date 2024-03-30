07c096615e7b22a675801623ad6ef7eddb540c0f
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-30 07:28:19 -0400

    add sep mint page

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-
-from .web3_test import is_w3_connected
 from .web3_qn import get_tx_data
+
 from .abi import abi
 
 app = Flask(__name__)
@@ -9,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    w3_response = is_w3_connected()
+    w3_response = "No func"
     return render_template("homePage.html", name="Dawg", w3_connect=w3_response)
 
 
@@ -45,4 +44,35 @@ def mint_post_callback():
     print(post_data)
     tx_hash = post_data["untrustedData"]["transactionId"]
 
-    return render_template("frame2.html", tx_hash=tx_hash), 200
+    return render_template("scanFrame.html", tx_hash=tx_hash), 200
+
+
+@app.route("/sep_mint-post", methods=["POST"])
+def sep_mint_post():
+    post_data = request.json
+    print("NICE CLICK")
+    print(post_data)
+    user_acct = post_data["untrustedData"]["address"]
+    print(user_acct + " IS THE ADDRESS")
+    tx_data = get_tx_data(user_acct)
+    print(tx_data + " IS THE TX DATA")
+    data = {
+        "chainId": "eip155:84532",
+        "method": "eth_sendTransaction",
+        "params": {
+            "abi": abi(),
+            "to": "0x12eb6Eb818C485eEc3a5fFa52860686802401aCA",
+            "data": tx_data,
+            "value": 5000000000000000,
+        },
+    }
+    return jsonify(data), 200
+
+
+@app.route("/sep_mint-post-callback", methods=["POST"])
+def sep_mint_post_callback():
+    post_data = request.json
+    print(post_data)
+    tx_hash = post_data["untrustedData"]["transactionId"]
+
+    return render_template("sepScanFrame.html", tx_hash=tx_hash), 200
---
+++ api/sep_alc.py
@@ -0,0 +1,27 @@
+import os
+from web3 import Web3
+from .abi import sep_abi
+
+
+CONTRACT_ABI = sep_abi()
+alc_base_sep_rpc = os.environ.get("ALCHEMY_BASE_SEP_RPC")
+
+my_acct = "0x9f3268BaA5a163Ce4fa3eE49F7AA7DAf2b97Fc8a"
+acct_2 = "0x585a6b5E1fA425eAEd742adb23607C6b8342949F"
+
+
+def get_tx_data(acct_address):
+    try:
+        w3 = Web3(Web3.HTTPProvider(alc_base_sep_rpc))
+        my_contract = w3.eth.contract(
+            address="0x12eb6Eb818C485eEc3a5fFa52860686802401aCA", abi=CONTRACT_ABI
+        )
+        build_tx = my_contract.functions.mintPill().build_transaction(
+            {"from": acct_address, "value": 5000000000000000}
+        )
+        tx_data = build_tx["data"]
+        return tx_data
+
+    except Exception as e:
+        print("Connection failed:", e)
+        return 0
