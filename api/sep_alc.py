69109d671949b52d5bf547081f4279b6584e69e2
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-30 08:04:49 -0400

    use alche rpc

--- api/sep_alc.py
+++ api/sep_alc.py
@@ -6,9 +6,6 @@ from .abi import sep_abi
 CONTRACT_ABI = sep_abi()
 alc_base_sep_rpc = os.environ.get("ALCHEMY_BASE_SEP_RPC")
 
-my_acct = "0x9f3268BaA5a163Ce4fa3eE49F7AA7DAf2b97Fc8a"
-acct_2 = "0x585a6b5E1fA425eAEd742adb23607C6b8342949F"
-
 
 def get_tx_data(acct_address):
     try:
--- api/web3_qn.py
+++ api/web3_qn.py
@@ -4,7 +4,7 @@ from .abi import abi
 
 
 CONTRACT_ABI = abi()
-qn_rpc = os.environ.get("QUICKNODE_BASE_RPC")
+qn_rpc = os.environ.get("ALCHEMY_BASE_RPC")
 
 my_acct = "0x9f3268BaA5a163Ce4fa3eE49F7AA7DAf2b97Fc8a"
 acct_2 = "0x585a6b5E1fA425eAEd742adb23607C6b8342949F"
@@ -17,7 +17,7 @@ def get_tx_data(acct_address):
             address="0xD09d72031Ce8212efD8928F1A3814E7F8da0fDfD", abi=CONTRACT_ABI
         )
         build_tx = my_contract.functions.safeTransferFrom(
-            acct_address, my_acct, 2
+            acct_address, my_acct, 3
         ).build_transaction({"from": acct_address})
         tx_data = build_tx["data"]
         return tx_data
