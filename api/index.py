95bac06f4ea91bf0249377d4f696c618f0c3fd29
lah-b-mac <bill@Bills-Mac-Studio.local>
2024-03-30 12:31:34 -0400

    clean up all pages

--- api/index.py
+++ api/index.py
@@ -1,68 +1,27 @@
 from flask import Flask, render_template, request, jsonify
 from .web3_qn import get_tx_data
 
-from .abi import abi, sep_abi
+from .abi import sep_abi
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    w3_response = "No func"
-    return render_template("homePage.html", name="Dawg", w3_connect=w3_response)
+    return render_template("homePage.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/mint-post", methods=["POST"])
-def mint_post():
-    post_data = request.json
-    print("NICE CLICK")
-    print(post_data)
-    user_acct = post_data["untrustedData"]["address"]
-    print(user_acct + " IS THE ADDRESS")
-    tx_data = get_tx_data(user_acct)
-    print(tx_data + " IS THE TX DATA")
-    data = {
-        "chainId": "eip155:8453",
-        "method": "eth_sendTransaction",
-        "params": {
-            "abi": abi(),
-            "to": "0xD09d72031Ce8212efD8928F1A3814E7F8da0fDfD",
-            "data": tx_data,
-        },
-    }
-    return jsonify(data), 200
-
-
-@app.route("/mint-post-callback", methods=["POST"])
-def mint_post_callback():
-    post_data = request.json
-    print(post_data)
-    tx_hash = post_data["untrustedData"]["transactionId"]
-
-    return render_template("scanFrame.html", tx_hash=tx_hash), 200
-
-
-@app.route("/sep")
+@app.route("/sep-mint")
 def sep_home():
-    w3_response = "No func"
-    return render_template("sepHomePage.html", name="Dawg", w3_connect=w3_response)
+    return render_template("sepMintFrame.html")
 
 
 @app.route("/sep-mint-post", methods=["POST"])
 def sep_mint_post():
     post_data = request.json
-    print("NICE CLICK")
-    print(post_data)
     user_acct = post_data["untrustedData"]["address"]
     print(user_acct + " IS THE ADDRESS")
     tx_data = get_tx_data(user_acct)
-    print("TX DATA")
-    print(tx_data)
     data = {
         "chainId": "eip155:84532",
         "method": "eth_sendTransaction",
@@ -70,7 +29,6 @@ def sep_mint_post():
             "abi": sep_abi(),
             "to": "0x12eb6Eb818C485eEc3a5fFa52860686802401aCA",
             "data": tx_data,
-            "value": str(5000000000000000),
         },
     }
     return jsonify(data), 200
@@ -81,5 +39,4 @@ def sep_mint_post_callback():
     post_data = request.json
     print(post_data)
     tx_hash = post_data["untrustedData"]["transactionId"]
-
     return render_template("sepScanFrame.html", tx_hash=tx_hash), 200
--- api/sep_alc.py
+++ api/sep_alc.py
@@ -10,12 +10,11 @@ alc_base_sep_rpc = os.environ.get("ALCHEMY_BASE_SEP_RPC")
 def get_tx_data(acct_address):
     try:
         w3 = Web3(Web3.HTTPProvider(alc_base_sep_rpc))
-        print(f"Connected to base sep {w3.is_connected()}")
         my_contract = w3.eth.contract(
-            address="0x12eb6Eb818C485eEc3a5fFa52860686802401aCA", abi=CONTRACT_ABI
+            address="0x40ea87a0dffad828778eb248156df01a358f541b", abi=CONTRACT_ABI
         )
         build_tx = my_contract.functions.mintPill().build_transaction(
-            {"from": acct_address, "value": 5000000000000000}
+            {"from": acct_address}
         )
         tx_data = build_tx["data"]
         return tx_data
